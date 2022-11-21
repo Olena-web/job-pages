14b4cf5a355bbe0e17ed04dd17aae4114198fa1f
Olena Honcharuk <gasmin07@gmail.com>
2022-11-22 00:29:35 +0200

    fix: another variant for diff of dates

--- src/JobCard/JobCard.tsx
+++ src/JobCard/JobCard.tsx
@@ -49,15 +49,15 @@ constructor(props: ApiData) {
     };
     
 }
-Data = (data: string) => {
+Data = (createdData: string, updatedData: string) => {
     const one_day=1000*60*60*24;
-    const d = new Date((data));
-    const dayToday = new Date().getTime();
-    const diff = Math.ceil((dayToday - d.getTime())/(one_day));
+    const d1 = new Date(createdData).getTime();
+    const d2 = new Date(updatedData).getTime();
+    const diff = Math.ceil((d2-d1)/(one_day));
     if (diff === 0) {
        return (<div>"Posted today"</div>);
     } else if (diff === 1) {
-        return (<div>"Posted yesetrday"</div>);
+        return (<div>"Posted yesterday"</div>);
     } else {
         return (<div>"Posted {diff} days ago"</div>);
     }
@@ -87,7 +87,7 @@ Data = (data: string) => {
           </div>
           <div className="marks">
             <BookmarkBorderOutlinedIcon sx={{ maxWidth: 17 }}/>
-            {this.Data(this.props.updatedAt)}
+            {this.Data(this.props.createdAt, this.props.updatedAt)}
             </div>
         </Card>
         )
