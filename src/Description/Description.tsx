c31e75be9b0148b451bd93bb6b9a366d72bdb87d
Honcharuk Olena <gasmin07@gmail.com>
2022-11-23 16:14:09 +0200

    feat: add style for description

--- src/Description/Description.tsx
+++ src/Description/Description.tsx
@@ -1,11 +1,13 @@
+import { fontStyle } from "@mui/system";
+
 export const Description = (data: string) => {
     const formattedDescription = data.split(`\n`).map((item, index) => {
       if(item.includes(':'))
        {
-
+        console.log(item);    
         return (
           <div key={index}>
-            <h4 className='description-title'>{item}</h4>
+            <h4 className='description-title' style = {{ fontWeight: 'bold'}}>{item}</h4>
           </div>
         );
       }
