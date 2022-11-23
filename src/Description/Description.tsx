f30886a63bb3dd246cb22097e6a38729fd4a8660
Honcharuk Olena <gasmin07@gmail.com>
2022-11-23 15:58:02 +0200

    feat: add style for details page

--- src/Description/Description.tsx
+++ src/Description/Description.tsx
@@ -1,7 +1,24 @@
 export const Description = (data: string) => {
     const formattedDescription = data.split(`\n`).map((item, index) => {
-        return <p key={index}>{item}</p>;
-        });
+      if(item.includes(':'))
+       {
+
+        return (
+          <div key={index}>
+            <h4 className='description-title'>{item}</h4>
+          </div>
+        );
+      }
+      else {
+        return (
+          <div key={index}>
+            <p className='description-text'>{item}</p>
+          </div>
+        );
+      }
+    });
+
+       
 
   return (
     <div>
--- src/JobDetails/JobDetails.tsx
+++ src/JobDetails/JobDetails.tsx
@@ -5,6 +5,8 @@ import CardContent from '@mui/material/CardContent';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 import RoomOutlinedIcon from '@mui/icons-material/RoomOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
+import EuroSymbolIcon from '@mui/icons-material/EuroSymbol';
 
 import { ApiData } from '../types';
 import { useParams, Link } from 'react-router-dom';
@@ -39,34 +41,47 @@ export const JobDetails =() => {
         <div className='job-details-page'>
           <div className='job-details'>
             <header>
-              <h1>JOB DETAILS</h1>
-              <BookmarkBorderOutlinedIcon sx={{ maxWidth: 17 }}/>
-              <button> Save to my list</button>
-              <ShareOutlinedIcon sx={{ maxWidth: 17 }}/>
-              <button> Share </button>
-              <div className='separator'></div>
+              <h2>JOB DETAILS</h2>
+              <div className='save-share'>
+                <BookmarkBorderOutlinedIcon sx={{ maxWidth: 17 }}/>
+                <div> Save to my list</div>
+                <ShareOutlinedIcon sx={{ maxWidth: 17 }}/>
+                <div> Share </div>
+              </div>
             </header>
-           <button>Apply now</button>
+              <div className='separator'></div>
+           <button className='apply'>Apply now</button>
             <div className='details'>
                <CardContent sx={{ maxWidth: 823 }} className='content'>
-                <p>{itemToShow?.title}</p> 
-                <div className='salary'>
-                  {itemToShow?.salary} Brutto, per year
+                <div className='title'>
+                  <p className='title-name'>{itemToShow?.title}</p> 
+                  <div className='salary'>
+                    <span>
+                    <EuroSymbolIcon sx={{ maxWidth: 17 }}/> 
+                    {itemToShow?.salary} 
+                    </span>
+                    
+                    <span>Brutto, per year</span>
+                  </div>
                 </div>
-            {Data(itemToShow.createdAt, itemToShow.updatedAt)}
+                <div className='data'>
+                   {Data(itemToShow.createdAt, itemToShow.updatedAt)}
+                </div>
             <div className='description'>
+            
              { Description(itemToShow?.description)}
             </div>
-            <button>Apply now</button>
+            <button className='apply'>Apply now</button>
           <div className='additional'>
             <h2> Additional info </h2>
+            <div className='separator'></div>
             <h3>Employment type</h3>
-            <div>
+            <div className='employment_type'>
               <p>{itemToShow?.employment_type[0]}</p>
               <p>{itemToShow?.employment_type[1]}</p>
             </div>
             <h3>Benefits</h3>
-            <div>
+            <div className='benefits'>
               <p>{itemToShow?.benefits[0]}</p>
               <p>{itemToShow?.benefits[1]}</p>
             </div>
@@ -75,15 +90,18 @@ export const JobDetails =() => {
           
           <div className='images'>
             <h2>Attached images</h2>
+            <div className='separator'></div>
             <div className='image-wrapper'>
-              <img src={itemToShow?.pictures[0]} alt="img1" style={{ width: 85, height: 85 }} />
-              <img src={itemToShow?.pictures[1]} alt="img2" style={{ width: 85, height: 85 }} />
-              <img src={itemToShow?.pictures[2]} alt="img3" style={{ width: 85, height: 85 }} />
+              <img src={itemToShow?.pictures[0]} alt="img1" style={{ width: 234, height: 156 }} />
+              <img src={itemToShow?.pictures[1]} alt="img2" style={{ width: 234, height: 156 }} />
+              <img src={itemToShow?.pictures[2]} alt="img3" style={{ width: 234, height: 156 }} />
             </div>
           </div>
           </CardContent> 
         </div>
-        <Link to="/">RETURN TO JOB BOARD</Link> 
+        <Link className='return' to="/" >  
+        <ChevronLeftOutlinedIcon/>
+        RETURN TO JOB BOARD</Link>
           </div>
           <aside>
             <div className='company'>
